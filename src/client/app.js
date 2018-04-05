f1dda592cadaa6d038715bd9fd88ec7125971a1e
Conner Aiken <conner@fittedtech.com>
2018-04-04 22:22:53 -0700

    Init commit

---
+++ src/client/app.js
@@ -0,0 +1,13 @@
+import react from 'react';
+class App extends React.Component {
+ 
+  render() {
+      return (
+         <h1>Hello from React!</h1>
+      );
+  }
+}
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);

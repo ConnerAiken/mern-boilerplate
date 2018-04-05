688c19631cc50e3c1613d43fce51d6e79c8eec64
Conner Aiken <conner@fittedtech.com>
2018-04-04 22:40:09 -0700

    Part 1

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const entryFile = path.resolve(__dirname, 'src', 'client', 'app.js');
+const outputDir = path.resolve(__dirname, 'public');
+module.exports = {
+ entry: entryFile,
+ output: {
+      filename: 'bundle.js',
+      path: outputDir
+ },
+ module: {
+      rules: [
+       
+        {
+           test: /\.(js|jsx)$/,
+           loader: 'babel-loader',
+           exclude: /node_modules/
+         },
+         {
+           test: /\.(scss|css)$/,
+           use: [
+              {
+                loader: 'style-loader'
+              }, 
+              { 
+                loader: 'css-loader',
+              }, 
+              { 
+                 loader: 'sass-loader' 
+              }
+            ]
+          }
+       ]
+ }
+};
